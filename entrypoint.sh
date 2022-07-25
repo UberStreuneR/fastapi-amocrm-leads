764798be819729da06c592e3011733aff8be15e7
Олег Стецюк <o.stetsyuk@kanalservis.ru>
2022-07-25 17:56:40 +0300

    Added entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+[ -z $DB_HOST ] && DB_HOST="postgres"
+[ -z $DB_PORT ] && DB_PORT="5432"
+
+echo "Waiting for PostgreSQL..."
+while ! nc -z $DB_HOST $DB_PORT; do
+    sleep 0.1
+done
+echo "PostgreSQL started"
+
+exec "$@"
