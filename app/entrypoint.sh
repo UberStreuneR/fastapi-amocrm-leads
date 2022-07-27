658a7674119546a2744b43880a6d007b821847f2
Олег Стецюк <o.stetsyuk@kanalservis.ru>
2022-07-27 08:49:34 +0300

    Docker fix

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 [ -z $DB_HOST ] && DB_HOST="contact_level_db"
-[ -z $DB_PORT ] && DB_PORT="5433"
+[ -z $DB_PORT ] && DB_PORT="5432"
 
 echo "Waiting for PostgreSQL..."
 while ! nc -z $DB_HOST $DB_PORT; do
