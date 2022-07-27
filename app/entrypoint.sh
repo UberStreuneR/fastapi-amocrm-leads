414820b9b50a7fe70b6eebada3737b3ba70452d5
Олег Стецюк <o.stetsyuk@kanalservis.ru>
2022-07-27 08:21:10 +0300

    Changed postgres port

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 [ -z $DB_HOST ] && DB_HOST="postgres"
-[ -z $DB_PORT ] && DB_PORT="5432"
+[ -z $DB_PORT ] && DB_PORT="5433"
 
 echo "Waiting for PostgreSQL..."
 while ! nc -z $DB_HOST $DB_PORT; do
