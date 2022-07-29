beab6c36ca1144ca58e37f49c8eee7a3af9dd27a
Олег Стецюк <o.stetsyuk@kanalservis.ru>
2022-07-29 15:06:25 +0300

    Fix

--- app/settings/utils.py
+++ app/settings/utils.py
@@ -107,11 +107,13 @@ class CompanyManager(EntityManager):
             self._update_leads_values = []
 
     def set_field_if_different(self, company_id: int, field_id: int, value: int, company_data):
-        print(f"\n\n{company_id}\n{field_id}\n{value}\n{company_data}")
+        # print(f"\n\n{company_id}\n{field_id}\n{value}\n{company_data}")
 
         for custom_field in company_data['custom_fields_values']:
             if int(custom_field['field_id']) == int(field_id):
                 if str(custom_field['values'][0]['value']) != str(value):
+                    print(
+                        f"\n\nSAME VALUE: {custom_field['values'][0]['value']} = {value}\n\n")
                     for update_value in self._update_values:
                         if update_value['id'] == company_id:
                             update_value['custom_fields_values'].append(
@@ -120,6 +122,8 @@ class CompanyManager(EntityManager):
                     self._update_values.append(
                         {"id": company_id, "field_id": field_id, "value": value})
                     return
+        self._update_values.append(
+            {"id": company_id, "field_id": field_id, "value": value})
     # comparison_value является либо суммой, либо количеством
 
     def apply_one_status_setting(self, company_id: int, status_setting: StatusSetting, comparison_value: int, company_data):
@@ -216,7 +220,9 @@ class ContactManager(EntityManager):
                             return
                     self._update_values.append(
                         {"id": contact_id, "field_id": field_id, "value": value})
-                    break
+                    return
+        self._update_values.append(
+            {"id": contact_id, "field_id": field_id, "value": value})
 
     # comparison_value является либо суммой, либо количеством
     def apply_one_status_setting(self, contact_id: int, status_setting: StatusSetting, comparison_value: int, contact_data):
