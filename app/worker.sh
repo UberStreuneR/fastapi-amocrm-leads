6022364662d214dfc3a20fc9ea42cd3235c0dcc6
Олег Стецюк <o.stetsyuk@kanalservis.ru>
2022-08-01 12:51:38 +0300

    Added worker bash script

---
+++ app/worker.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+[ -z $RABBIT_HOST ] && RABBIT_HOST="rabbitmq"
+[ -z $RABBIT_PORT ] && RABBIT_PORT="5672"
+
+echo "Waiting for RabbitMQ..."
+while ! nc -z $RABBIT_HOST $RABBIT_PORT; do
+    sleep 0.1
+done
+echo "RabbitMQ started"
+celery -A app.worker worker --loglevel=INFO -Q settings
+exec "$@"
