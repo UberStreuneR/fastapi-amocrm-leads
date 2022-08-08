ea4fa14950ed2b4a33271d29e751b298c9a72943
Олег Стецюк <o.stetsyuk@kanalservis.ru>
2022-08-08 11:23:15 +0300

    logging

--- app/amocrm/base.py
+++ app/amocrm/base.py
@@ -8,8 +8,16 @@ import time
 
 from typing import Callable, Union, List, Generator
 
-from app.integrations.deps import get_logger
-logger = get_logger()
+import logging
+import sys
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s:%(name)s: %(message)s",
+    level=logging.DEBUG,
+    datefmt="%H:%M:%S",
+    stream=sys.stderr,
+)
+logger = logging.getLogger("Sockets")
+logging.getLogger("chardet.charsetprober").disabled = True
 
 
 class AmoCRM:
